7c1ea06ef41945abcad0d27db41096fc412fcd14
Morton <morton.nari@yahoo.com.au>
2020-04-24 02:37:55 +1000

    final

---
+++ dist/script.d573be0b.js
@@ -0,0 +1,424 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  var error;
+  for (var i = 0; i < entry.length; i++) {
+    try {
+      newRequire(entry[i]);
+    } catch (e) {
+      // Save first error but execute all entries
+      if (!error) {
+        error = e;
+      }
+    }
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  parcelRequire = newRequire;
+
+  if (error) {
+    // throw error from earlier, _after updating parcelRequire_
+    throw error;
+  }
+
+  return newRequire;
+})({"js/script.js":[function(require,module,exports) {
+// import "bootstrap";
+// document.getElementById('btn-1').addEventListener('click', progressBar);
+window.onload = function () {
+  // const jokesNumbers = document.querySelector('input[type="number"]').value;
+  xhr = new XMLHttpRequest();
+  xhr.open('GET', "http://pb-api.herokuapp.com/bars", true);
+
+  xhr.onload = function () {
+    if (this.status === 200) {
+      var progressNum = JSON.parse(this.responseText);
+      var limit = progressNum.limit;
+      console.log(limit);
+      var btnBar = '';
+      progressNum.buttons.forEach(function (element, index) {
+        btnBar += "<button type=\"button\" class=\"btn btn-secondary\" id=\"btn-".concat(index, "\" onclick=\"addToBar(").concat(element, " , ").concat(limit, ")\">").concat(element, "</button>");
+        console.log(btnBar);
+      });
+      document.getElementById('btn-ctr').innerHTML = btnBar;
+      console.log(progressNum); // document.getElementById('btn-1').textContent = progressNum.buttons[0];
+      // document.getElementById('btn-2').textContent = progressNum.buttons[1];
+      // document.getElementById('btn-3').textContent = progressNum.buttons[2];
+      // document.getElementById('bar-1').style.width = progressNum.bars[0] * 10 + 'px';
+      // document.getElementById('bar-2').style.width = progressNum.bars[1] * 10 + 'px';
+      // document.getElementById('bar-3').style.width = progressNum.bars[2] * 10 + 'px';
+      // document.getElementById('bar-1').textContent = progressNum.bars[0] ;
+      // document.getElementById('bar-2').textContent = progressNum.bars[1] ;
+      // document.getElementById('bar-3').textContent = progressNum.bars[2] ;
+
+      var prBar = '';
+      progressNum.bars.forEach(function (element, index) {
+        prBar += "<div class=\"row my-3 \">\n                <div class=\"col-12 d-flex justify-content-center\" >\n                    <div class=\"progress\" style=\"height: 40px;width:".concat(limit, "px;\" >\n                        <div  aria-valuenow=\"").concat(element, "\" aria-valuemin=\"0\" aria-valuemax=\"").concat(limit, "\" id=\"").concat(element, "\" role=\"progressbar\"  \n                        class=\"progress-bar bg-info progress-bar-striped progress-bar-animated\" style=\"width:").concat(element / limit * 100, "%;\" >").concat(element, "</div>\n                    </div>\n                </div>\n            </div>");
+      });
+      document.getElementById('proBar').innerHTML = prBar;
+      var selBar = '';
+      var selBarMain = "\t\n            <select onchange= \"barSelect(value)\"  id=\"select-ctr\" class=\"form-control pr-5 mr-3\" ></select>\n            ";
+      document.getElementById('select-main-ctr').innerHTML = selBarMain;
+      progressNum.bars.forEach(function (elem, index) {
+        selBar += "<option   id=\"sel".concat(elem, "\" data=\"").concat(elem, "\" value = \"").concat(elem, "\" >Bar ").concat(index + 1, "</option>");
+      });
+      document.getElementById('select-ctr').innerHTML = selBar;
+      var checkedBar = document.querySelectorAll('.progress-bar');
+      checkedBar[0].classList.add('active'); // const output = `
+      // `;
+      // document.getElementById('buttons').innerHTML = output;
+      // console.log(progressNum);
+    }
+  };
+
+  xhr.send();
+  var addToBar;
+  var barSelect;
+}; // document.getElementById('bar-1').focus(); 
+
+
+barSelect = function barSelect(value) {
+  var el = document.querySelectorAll('.progress-bar');
+  console.log(el);
+  el.forEach(function (elem) {
+    if (elem.classList.contains('active')) {
+      elem.classList.remove('active');
+    }
+
+    document.getElementById(value).classList.add('active');
+  });
+};
+
+addToBar = function addToBar(params, limit) {
+  //  document.querySelector('.active').style.width = + params +'%' ;.
+  var barWidth = document.querySelector('.active').textContent;
+  barWidth = parseInt(barWidth);
+
+  if (barWidth >= 0) {
+    barWidth += params;
+    document.querySelector('.active').textContent = barWidth;
+    document.querySelector('.active').style.width = barWidth / limit * 100 + '%';
+  }
+
+  if (barWidth < 0) {
+    document.querySelector('.active').textContent = 0;
+  }
+
+  if (barWidth > limit) {
+    document.querySelector('.active').classList.remove('bg-info');
+    document.querySelector('.active').classList.add('bg-danger');
+  }
+
+  if (barWidth <= limit) {
+    document.querySelector('.active').classList.remove('bg-danger');
+    document.querySelector('.active').classList.add('bg-info');
+  }
+
+  if (barWidth <= 0) {
+    document.querySelector('.active').style.width = 5 + '%';
+  }
+
+  console.log(limit);
+  console.log(params);
+};
+},{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var global = arguments[3];
+var OVERLAY_ID = '__parcel__error__overlay__';
+var OldModule = module.bundle.Module;
+
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    data: module.bundle.hotData,
+    _acceptCallbacks: [],
+    _disposeCallbacks: [],
+    accept: function (fn) {
+      this._acceptCallbacks.push(fn || function () {});
+    },
+    dispose: function (fn) {
+      this._disposeCallbacks.push(fn);
+    }
+  };
+  module.bundle.hotData = null;
+}
+
+module.bundle.Module = Module;
+var checkedAssets, assetsToAccept;
+var parent = module.bundle.parent;
+
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = "" || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59464" + '/');
+
+  ws.onmessage = function (event) {
+    checkedAssets = {};
+    assetsToAccept = [];
+    var data = JSON.parse(event.data);
+
+    if (data.type === 'update') {
+      var handled = false;
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          var didAccept = hmrAcceptCheck(global.parcelRequire, asset.id);
+
+          if (didAccept) {
+            handled = true;
+          }
+        }
+      }); // Enable HMR for CSS by default.
+
+      handled = handled || data.assets.every(function (asset) {
+        return asset.type === 'css' && asset.generated.js;
+      });
+
+      if (handled) {
+        console.clear();
+        data.assets.forEach(function (asset) {
+          hmrApply(global.parcelRequire, asset);
+        });
+        assetsToAccept.forEach(function (v) {
+          hmrAcceptRun(v[0], v[1]);
+        });
+      } else if (location.reload) {
+        // `location` global exists in a web worker context but lacks `.reload()` function.
+        location.reload();
+      }
+    }
+
+    if (data.type === 'reload') {
+      ws.close();
+
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+      removeErrorOverlay();
+    }
+
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
+      removeErrorOverlay();
+      var overlay = createErrorOverlay(data);
+      document.body.appendChild(overlay);
+    }
+  };
+}
+
+function removeErrorOverlay() {
+  var overlay = document.getElementById(OVERLAY_ID);
+
+  if (overlay) {
+    overlay.remove();
+  }
+}
+
+function createErrorOverlay(data) {
+  var overlay = document.createElement('div');
+  overlay.id = OVERLAY_ID; // html encode message and stack trace
+
+  var message = document.createElement('div');
+  var stackTrace = document.createElement('pre');
+  message.innerText = data.error.message;
+  stackTrace.innerText = data.error.stack;
+  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
+  return overlay;
+}
+
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return [];
+  }
+
+  var parents = [];
+  var k, d, dep;
+
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(k);
+      }
+    }
+  }
+
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+
+  return parents;
+}
+
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return;
+  }
+
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+
+function hmrAcceptCheck(bundle, id) {
+  var modules = bundle.modules;
+
+  if (!modules) {
+    return;
+  }
+
+  if (!modules[id] && bundle.parent) {
+    return hmrAcceptCheck(bundle.parent, id);
+  }
+
+  if (checkedAssets[id]) {
+    return;
+  }
+
+  checkedAssets[id] = true;
+  var cached = bundle.cache[id];
+  assetsToAccept.push([bundle, id]);
+
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    return true;
+  }
+
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAcceptCheck(global.parcelRequire, id);
+  });
+}
+
+function hmrAcceptRun(bundle, id) {
+  var cached = bundle.cache[id];
+  bundle.hotData = {};
+
+  if (cached) {
+    cached.hot.data = bundle.hotData;
+  }
+
+  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+    cached.hot._disposeCallbacks.forEach(function (cb) {
+      cb(bundle.hotData);
+    });
+  }
+
+  delete bundle.cache[id];
+  bundle(id);
+  cached = bundle.cache[id];
+
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    cached.hot._acceptCallbacks.forEach(function (cb) {
+      cb();
+    });
+
+    return true;
+  }
+}
+},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/script.js"], null)
+//# sourceMappingURL=/script.d573be0b.js.map
